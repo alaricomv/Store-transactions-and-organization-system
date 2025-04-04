df18346550f6405f4e41ab3050312bd165f9bb18
Alarico Mercado <alarico.mercado@gmail.com>
2025-04-03 17:51:20 -0700

    Calculator and last transactions partial components created

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -5,13 +5,15 @@ import { CommonModule } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import { LOCALE_ID } from '@angular/core';
+import { CalculatorComponent } from "../../partials/calculator/calculator.component";
+import { LastTransactionsComponent } from "../../partials/last-transactions/last-transactions.component";
 
 registerLocaleData(localeEs); // Register Spanish locale
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalculatorComponent, LastTransactionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   providers: [{ provide: LOCALE_ID, useValue: 'es' }] // Set locale to Spanish
---
+++ frontend/src/app/components/partials/last-transactions/last-transactions.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Transaction } from '../../../shared/models/transaction';
+import { TransactionService } from '../../../services/transaction.service';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+registerLocaleData(localeEs); // Register Spanish locale
+
+@Component({
+  selector: 'app-last-transactions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './last-transactions.component.html',
+  styleUrl: './last-transactions.component.css',
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }]
+})
+export class LastTransactionsComponent {
+
+    lasttransactions:Transaction[] = [];
+  
+    constructor(private transactionService:TransactionService) {
+      this.lasttransactions = transactionService.getLastthree();
+  
+    }
+
+}
