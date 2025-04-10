5df92572ffcac85ef4e19306ffc567c9b4d07634
Alarico Mercado <alarico.mercado@gmail.com>
2025-04-09 19:40:35 -0700

    Routing working, Page for a single transaction created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { TransactionPageComponent } from './components/pages/transaction-page/transaction-page.component'; 
+import { CalculatorPageComponent } from './components/pages/calculator-page/calculator-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'transaction/:id', component: TransactionPageComponent }, // Transactions page
+  { path: 'calculator', component: CalculatorPageComponent }, // Transactions page
+  { path: '**', redirectTo: '' } // Redirect unknown routes to the home page
+];
