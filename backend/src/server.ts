a328fedc9ef4800bc90db7b0664400c417d44fd6
Alarico Mercado <alarico.mercado@gmail.com>
2025-04-09 18:27:03 -0700

    Added transaction calls

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,6 +12,22 @@ app.get('/api/transactions', (req, res) => {
     res.send(sample_transactions);
 })
 
+app.get('/api/transactions/lasttransactions', (req, res) => {
+    const lastTransactions = sample_transactions
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()) // Sort by date descending
+        .slice(0, 3) // Get the last 3 transactions
+        .reverse(); // Reverse the order to get the last 3 transactions in ascending order
+
+    res.send(lastTransactions);
+})
+
+app.get('/api/transactions/:id', (req, res) => {
+    const transactionid = req.params.id;
+    const transaction = sample_transactions.find(transaction => transaction.id == transactionid);
+    res.send(transaction);
+})
+
+
 const PORT = 5000;
 
 app.listen(PORT,() => {
