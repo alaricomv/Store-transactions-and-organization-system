a081f6d4915dc96ebb42ecac2cacef73dca0ac37
Alarico Mercado <alarico.mercado@gmail.com>
2025-04-23 00:00:33 -0700

    More calls to database, insert new user with hashing, install express

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
 dotenv.config(); // Load environment variables from .env file
 
 const pool = mysql.createPool({
@@ -9,10 +10,22 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-async function getUsers() {
+export async function getUsers() {
     const [rows] = await pool.query('SELECT * FROM users');
     return rows;
 }
 
-const notes = await getUsers(); // Call the function to get users
-console.log(notes); // Output the result of the query
+export async function getUserById(id) {
+    const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+    return rows[0];
+}
+
+export async function createUser(company_name, branch_name, password, email) {
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const [result] = await pool.query('INSERT INTO users (company_name, branch_name, password, email) VALUES (?, ?, ?, ?)', [company_name, branch_name, hashedPassword, email]);
+    const id = result.insertId; // Get the ID of the newly created user
+    return getUserById(id); // Return the newly created user
+}
+
+const result = await createUser('test company', 'test branch', 'password', 'testmail@mail.com'); // Call the function to get users
+console.log(result); // Output the result of the query
