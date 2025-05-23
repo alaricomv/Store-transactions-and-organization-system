37f06797bf67f13102461c3a174fc92e88931c73
Alarico Mercado <alarico.mercado@gmail.com>
2025-05-22 17:57:35 -0700

    Update to angular 18 working, error appearing in a toastr instance but not affecting the run

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/partials/header/header.component";
 import { HomeComponent } from "./components/pages/home/home.component";
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HeaderComponent, HomeComponent, HttpClientModule, ReactiveFormsModule],
+  imports: [RouterLink, RouterOutlet, HeaderComponent, HomeComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
