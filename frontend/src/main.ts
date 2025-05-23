53752f2fa702e04b9375fd700f630067978b5757
Alarico Mercado <alarico.mercado@gmail.com>
2025-05-22 18:53:57 -0700

    Slider in home page working using swiper

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,6 @@
+import { register } from 'swiper/element/bundle';
+register();
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
