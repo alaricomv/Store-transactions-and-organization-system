9dc1c9724f15c0be45169ebd38f4e69ecec8257f
Alarico Mercado <alarico.mercado@gmail.com>
2025-06-16 15:29:28 -0700

    Changed back to use localhost frontend

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,5 +1,4 @@
-//Change this depending on the instance
-const BASE_URL = 'http://3.15.198.141:8080';
+const BASE_URL = 'http://localhost:8080';
 
 export const TRANSACTIONS_URL = `${BASE_URL}/transactions`;
 export const LAST_TRANSACTIONS_URL = BASE_URL + '/lasttransactions';
