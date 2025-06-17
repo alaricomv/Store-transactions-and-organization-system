7898945ae0eb80a056a2191eb15247e4fe04b9c4
Alarico Mercado <alarico.mercado@gmail.com>
2025-06-16 18:06:16 -0700

    Version running in the server, changed the base url in the frontend urls.ts

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = '/api';
 
 export const TRANSACTIONS_URL = `${BASE_URL}/transactions`;
 export const LAST_TRANSACTIONS_URL = BASE_URL + '/lasttransactions';
